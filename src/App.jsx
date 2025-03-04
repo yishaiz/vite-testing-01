46f97e7aec29003bfb821e244817329fb07f7fb5
yishai <yishai5555@gmail.com>
2025-03-04 13:40:28 +0200

    02-fix-css

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import { useState } from 'react';
 
 function App() {
   const [buttonColor, setButtonColor] = useState('red');
-  const nextColor = buttonColor === 'red' ? 'blue' : 'red';
+  // const nextColor = buttonColor === 'red' ? 'blue' : 'red';
 
   return (
     <div>
-      <button className={buttonColor} onClick={setButtonColor(nextColor)}>
-        Change to {nextColor}
+      <button
+        className={buttonColor}
+        onClick={() =>
+          setButtonColor((prevColor) => (prevColor === 'red' ? 'blue' : 'red'))
+        }
+      >
+        Change to {buttonColor === 'red' ? 'blue' : 'red'}
       </button>
     </div>
   );
